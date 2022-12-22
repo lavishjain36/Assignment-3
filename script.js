4ccc5eba95230919dc95834aaca099345f1416c4
Lavish <lavishjain36@gmail.com>
2022-12-22 12:22:27 +0530

    Add files via upload
    Added Assignment-1

---
+++ script.js
@@ -0,0 +1,32 @@
+Question:
+// Answer
+// Browser.js :
+It is mainly used for client -side application
+It is a used for frontend
+Browser.js will limited access 
+ 
+in Browser.js
+window,location,document
+ 
+Browser.js=>
+chrome-v8 engine,FIrefox=>Spider monkey,Safari->javascript core 
+ 
+Browser.js is providing a GUI 
+Node.js=>
+it is outside of our browser.It used for writing server side application.
+used for backend
+it has full aceess to system=>
+read ,write ,or create an apis to develop backend on your machine
+require->is the object in node module include for you app
+ 
+Node.js runs in v8 engine is mainly used by google chrome
+Node.js doesnt' have UI(Headless)
+Node.js ->Everything is module ->its mandatory to import all modules in node.js
+ 
+ 
+ 
+// Output/Result:
+ 
+ 
+ 
+APplication programmig interface=>
